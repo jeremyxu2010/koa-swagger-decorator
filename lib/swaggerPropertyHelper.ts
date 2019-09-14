4901466eeec206ed723882735e9986166244b5ef
cody <wangkedi.wang@gmail.com>
2019-09-14 13:29:22 +0800

    start work for v2.0.0

--- lib/swaggerPropertyHelper.ts
+++ lib/swaggerPropertyHelper.ts
@@ -1,48 +1,26 @@
 /**
  * All other regular column types.
  */
-export type PropertyType = "string" | "number" | "boolean" | "array" | "object"
+export type PropertyType = "string" | "number" | "boolean" | "array" | "object";
 
 /**
- * 
+ *
  */
 export class PropertyOptions {
-    /**
-     * 
-     */
-    type: PropertyType = null;
-    /**
-     * 
-     */
-    required?: boolean = null;
-    /**
-     * 
-     */
-    example?: any = null;
-    /**
-     * 
-     */
-    description?: string = null;
-    /**
-     * 
-     */
-    items?: PropertyOptions = null;
-    /**
-     * 
-     */
+    type: PropertyType;
+    required?: boolean;
+    example?: any;
+    description?: string;
+    items?: PropertyOptions;
     properties?: any;
 }
 
-/**
- * 
- * @param source 
- */
-function deepClone(source:any) {
+function deepClone(source: any) {
     if (!source || typeof source !== 'object') {
-        return null;
+        return;
     }
-    var targetObj:any = source.constructor === Array ? [] : {};
-    for (var keys in source) {
+    const targetObj: any = source.constructor === Array ? [] : {};
+    for (const keys in source) {
         if (source.hasOwnProperty(keys)) {
             if (source[keys] && typeof source[keys] === 'object') {
                 targetObj[keys] = source[keys].constructor === Array ? [] : {};
@@ -55,11 +33,7 @@ function deepClone(source:any) {
     return targetObj;
 }
 
-/**
- * Made for empty class
- * @param constructor 
- */
-export function swaggerClass(constructor?: Function): Function {
+export function swaggerClass(): Function {
     return function (target: any, propertyKey: string, descriptor: PropertyDescriptor) {
         if (target.swaggerDocument == undefined) target.swaggerDocument = {};
         if (target.swaggerClass == undefined) target.swaggerClass = target;
@@ -67,14 +41,9 @@ export function swaggerClass(constructor?: Function): Function {
             target.swaggerClass = target;
             target.swaggerDocument = deepClone(target.swaggerDocument);
         }
-    }
-};
+    };
+}
 
-/**
- * 
- * @param type 
- * @param options 
- */
 export function swaggerProperty(options?: PropertyOptions): Function {
     return function (target: any, propertyKey: string, descriptor: PropertyDescriptor) {
         if (target.constructor.swaggerDocument == undefined) target.constructor.swaggerDocument = {};
@@ -84,5 +53,5 @@ export function swaggerProperty(options?: PropertyOptions): Function {
             target.constructor.swaggerDocument = deepClone(target.constructor.swaggerDocument);
         }
         target.constructor.swaggerDocument[propertyKey] = options;
-    }
-};
+    };
+}
